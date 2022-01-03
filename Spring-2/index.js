4afd216fe726edef11c000c95681084843c73f96
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2022-01-03 19:22:02 +0530

    Assignment Files

---
+++ Spring-2/index.js
@@ -0,0 +1,16 @@
+function gotoMenu()
+{
+    window.location.href="menu.html"
+}
+function login()
+{
+    window.location.href="login.html"
+}
+function signup()
+{
+    window.location.href="signup.html"
+}
+function home()
+{
+    window.location.href="index.html"
+}
